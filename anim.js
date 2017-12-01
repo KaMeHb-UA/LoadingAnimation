9ae425595def904b7acc7db0b08bd4976753a78c
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-12-01 18:13:04 +0200

    Using safe "colors" part, support for using text-anim-text model, added tests

--- anim.js
+++ anim.js
@@ -1,7 +1,21 @@
+const colors = require('colors/safe');
 class LoadingAnimation{
     constructor(text = '', props = 'blue bold'){
+        if (typeof text == 'string'){
+            if (text.length) this.textBefore = text + ' ';
+            this.textAfter = '';
+        } else if (Array.isArray(text)){
+            if (text[0]){
+                if (text[0].length) this.textBefore = text[0] + ' ';
+            } else this.textBefore = '';
+            if (text[1]){
+                if (text[1].length) this.textAfter = ' ' + text[1];
+            } else this.textAfter = '';
+        } else {
+            throw new TypeError('text is not of type String or Array');
+        }
         function resetIfColor(text, color){
-            if (typeof text[color] == 'string') return text[color]; else return text;
+            if (typeof colors[color]) return colors[color](text); else return text;
         }
         function colorize(str, colors){
             colors.forEach(color => {
@@ -9,8 +23,7 @@ class LoadingAnimation{
             })
             return str;
         }
-        this.constructedWith = text;
-        this.internalTimer = (function(){
+        this.internalTimer = (() => {
             var symbols = ((arr, colors) => {
                 arr.forEach((symbol, i) => {
                     arr[i] = colorize(symbol, colors);
@@ -18,19 +31,24 @@ class LoadingAnimation{
                 return arr;
             })(['⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽'], props.split(' '));
             var i = 0;
-            return setInterval(function(){
-                process.stdout.clearLine();
+            return setInterval(() => {
                 process.stdout.cursorTo(0);
-                process.stdout.write(text + ' ' + symbols[i++]);
+                process.stdout.write(this.textBefore + symbols[i++] + this.textAfter);
                 i &= symbols.length - 1;
             }, 50);
         })();
     }
-    stop(){
+    stop(stopText = false){
         clearInterval(this.internalTimer);
         process.stdout.clearLine();
         process.stdout.cursorTo(0);
-        process.stdout.write(this.constructedWith + ' ');
+        process.stdout.write(this.textBefore);
+        if (stopText && stopText.length){
+            console.log(stopText + this.textAfter);
+        } else {
+            process.stdout.moveCursor(-1, 0);
+            console.log(this.textAfter);
+        }
     }
 }
 module.exports = LoadingAnimation;
